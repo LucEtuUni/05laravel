e52628cc1e34b51a98df00d4c9402ec250b1456a
LucLagerige <luc.lagerige@etu.unilim.fr>
2023-10-19 18:59:26 +0200

    pain

--- projet/routes/web.php
+++ projet/routes/web.php
@@ -15,10 +15,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('accueil');
+    return view('welcome');
 });
-
-
 
 Route::get('/dashboard', function () {
     return view('dashboard');
